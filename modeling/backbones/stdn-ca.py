9b77ec2c6a1b3ef764c45580a7a432b755014864
tk59854 <61262004+tk59854@users.noreply.github.com>
2022-11-17 14:13:49 +0800

    Update stdn-ca.py

--- modeling/backbones/stdn-ca.py
+++ modeling/backbones/stdn-ca.py
@@ -4,17 +4,22 @@ import torch.nn.functional as F
 
 
 class DPConv(nn.Module):  # decoupled parallel
-    def __init__(self, spatial_conv, dim, height, kernel_size=3, padding=1, last=False, squeeze=4):
+    def __init__(self, spatial_conv, dim, reso, kernel_size=3, padding=1, last=False, squeeze=4):
         super(DPConv,  self).__init__()
 
         self.last = last
         self.spatial_conv = spatial_conv
+        self.identity = (type(spatial_conv[0]) is nn.Identity)
+        
         self.temporal_conv = nn.Sequential(
             nn.Conv1d(dim, dim//squeeze, kernel_size=kernel_size, padding=padding, bias=False), 
             nn.BatchNorm1d(dim//squeeze),
             nn.LeakyReLU(inplace=True), 
             nn.Conv1d(dim//squeeze, dim, kernel_size=kernel_size, padding=padding, bias=False),   
         )
+        
+        self.dim = dim
+        self.h, self.w = reso
     
     def forward(self, x):
         # x[n, s, c, p, w]
@@ -23,7 +28,7 @@ class DPConv(nn.Module):  # decoupled parallel
         identity = x
 
         spatial_f = self.spatial_conv(x.view(-1, c, p, w)).view(n, s, -1, p, w)
-
+        
         temporal_f = x.max(-1)[0].permute(0, 3, 2, 1).contiguous()  # [n, p, c, s]
         temporal_f = self.temporal_conv(temporal_f.view(-1, c, s)).view(n, p, c, s) + temporal_f
         temporal_f = temporal_f.permute(0, 3, 2, 1).contiguous()  # [n, s, c, p]
@@ -35,28 +40,44 @@ class DPConv(nn.Module):  # decoupled parallel
             return weighted_f
         else:
             return weighted_f, temporal_f
+    
+    def flops(self):
+        if self.identity:
+            s_flops = 0
+            print('--- global branch ---')
+        else:
+            print('--- local branch ---')
+            s_flops = 3*3*self.dim*self.dim*self.h*self.w * 30
+        
+        t_flops = 3*self.dim*self.dim/4*30*64 * 2 + 2 * self.dim*self.h*self.w  # [ phi_(t) ] + [ P*Q+P ]
+        
+        print('s_flops for dim: ({}, {}, {}) = {}'.format(self.dim, self.h, self.w, s_flops))
+        print('t_flops for dim: ({}, {}) = {}'.format(self.dim, self.h, t_flops))
+        print('total_flops for dim: ({}, {}, {}) = {}'.format(self.dim, self.h, self.w, t_flops+s_flops))
+        
+        return s_flops + t_flops
 
 
 class DPBlock(nn.Module):
-    def __init__(self, reso_h, dim, kernel_size=3, padding=1, last=False, **kwargs):
+    def __init__(self, reso, dim, kernel_size=3, padding=1, last=False, **kwargs):
         super().__init__()
 
         if not last:
             starts_ratiao = torch.Tensor([0, 1, 3.5, 3]).cumsum(axis=0) # ratiao=7.5
         else:
             starts_ratiao = torch.Tensor([0, 1, 2, 1.5, 1.5, 1.5]).cumsum(axis=0) # ratiao=7.5
-        starts = [int((reso_h/7.5)*_ratiao) for _ratiao in starts_ratiao]
+        starts = [int((reso[0]/7.5)*_ratiao) for _ratiao in starts_ratiao]
         self.part_l = [starts[i+1] - starts[i] for i in range(len(starts)-1)]
 
         global_conv = nn.Sequential(
             nn.Identity())
-        self.global_pconv = DPConv(global_conv, dim, reso_h, last=last)
+        self.global_pconv = DPConv(global_conv, dim, reso, last=last)
 
-        local_conv = nn.Sequential(
+        part_conv = nn.Sequential(
             nn.Conv2d(dim, dim, kernel_size=kernel_size, bias=False, padding=padding, **kwargs), 
             nn.LeakyReLU(inplace=True),)
-        self.local_pconv = nn.ModuleList(
-            [DPConv(local_conv, dim, _l, last=last) for _l in self.part_l])
+        self.part_pconv = nn.ModuleList(
+            [DPConv(part_conv, dim, reso=(_l, reso[1]), last=last) for _l in self.part_l])
 
         self.last = last
 
@@ -67,29 +88,42 @@ class DPBlock(nn.Module):
         
         if not self.last:
             global_f = self.global_pconv(x)
-            local_f = torch.cat([_p_conv(_f) for _p_conv, _f in zip(self.local_pconv, x_split)], -2)  # [n, s, c, h, w]
-            return global_f + local_f
+            part_f = torch.cat([_p_conv(_f) for _p_conv, _f in zip(self.part_pconv, x_split)], -2)  # [n, s, c, h, w]
+            return global_f + part_f
         else: 
             global_f, global_temporal_f = self.global_pconv(x)
 
-            part_outs = [_p_conv(_f) for _p_conv, _f in zip(self.local_pconv, x_split)]
-            local_f, part_temporal_f = [], []
+            part_outs = [_p_conv(_f) for _p_conv, _f in zip(self.part_pconv, x_split)]
+            part_f, part_temporal_f = [], []
             for _outs in part_outs:
-                local_f.append(_outs[0])
+                part_f.append(_outs[0])
                 part_temporal_f.append(_outs[1])
-            local_f = torch.cat(local_f, -2)
+            part_f = torch.cat(part_f, -2)
             part_temporal_f = torch.cat(part_temporal_f, -1)
 
-            spatial_f = local_f + global_f  # [n, s, c, h, w]
+            spatial_f = part_f + global_f  # [n, s, c, h, w]
             temporal_f = part_temporal_f + global_temporal_f  # [n, s, c, h]
             
             return spatial_f, temporal_f
+    
+    def flops(self):
+        flops_g = self.global_pconv.flops()
+        flops_p = 0
+        for _p in self.part_pconv:
+            flops_p += _p.flops()
+        
+        return flops_g + flops_p
 
 
 class LinkLayer(nn.Module):
     def __init__(self, in_channels, out_channels, stride, is_stem=False):
         super().__init__()
 
+        self.is_stem = is_stem
+        self.stride = stride
+        self.c_in = in_channels
+        self.c_out = out_channels
+        
         if is_stem:
             self.link_layer = nn.Sequential(
                 nn.Conv2d(in_channels, out_channels, kernel_size=5, stride=stride, padding=2, bias=False),
@@ -112,25 +146,38 @@ class LinkLayer(nn.Module):
         x = self.link_layer(x.view(-1, c, h, w))
         _, c, h, w = x.shape
         return x.view(n, s, c, h, w)
+    
+    def flops(self):
+        if self.is_stem:
+            flops = 5*5*self.c_in*self.c_out*64*44
+        elif self.stride == 2:
+            flops = 1*1*self.c_in*self.c_out*64*44
+        else:
+            flops = 1*1*self.c_in*self.c_out*32*22
+        print('link flops:', flops)
+        return flops
 
 
 class STDN(nn.Module):
-    def __init__(self, c_in=1, depths=[1, 1, 1], dims=[64, 128, 256], downsample_stride=[1, 2, 1]):
+    def __init__(self, c_in=1, depths=[1, 1, 1], dims=[64, 128, 256], downsample_stride=[1, 2, 2]):
         super().__init__()
 
-        self.height = 64
+        self.H = 64
+        self.W = 44
+        
         self.num_stages = len(depths)
 
         self.stages = nn.ModuleList() 
 
         dims.insert(0, c_in)
         for i in range(self.num_stages):
-            self.height = self.height // downsample_stride[i]
+            self.H = self.H // downsample_stride[i]
+            self.W = self.W // downsample_stride[i]
             
             blocks = []
             for j in range(depths[i]):
                 last = ((i == self.num_stages - 1) and (j == depths[i]-1))
-                blocks.append(DPBlock(reso_h=self.height, dim=dims[i+1], kernel_size=3, padding=1, last=last))
+                blocks.append(DPBlock(reso=(self.H, self.W), dim=dims[i+1], kernel_size=3, padding=1, last=last))
 
             self.stages.append(nn.Sequential(
                 LinkLayer(dims[i], dims[i+1], stride=downsample_stride[i], is_stem=(i==0)),
@@ -145,13 +192,30 @@ class STDN(nn.Module):
     def forward(self, x):
         out = self.forward_features(x)
         return out
+    
+    def get_backbone_flops(self):
+        all_flops = 0
+        stage_flops = 0
+        for i, _stage in enumerate(self.stages):
+            for child in _stage:
+                stage_flops += child.flops()
+            print('flops of stage {}={}'.format(i, stage_flops))
+            print('========================')
+            all_flops += stage_flops
+            stage_flops = 0
+        # all_flops = all_flops + 2*2*256*256*64 # FCs
+        print('flops of ALL = {}'.format(all_flops))
+        FLOPs = all_flops / 1e9
+        print('flops of ALL = {} G'.format(FLOPs))
+            
+
 
 
 if __name__ == '__main__':
     import time
     model = STDN(depths=[1, 1, 1], dims=[64, 128, 256], downsample_stride=[1, 2, 1])
     x = torch.randn((16, 30, 1, 64, 44))
-    # print(model)
+    model.get_backbone_flops()
     t = time.time()
     spacial, temporal = model(x)
     print(time.time() - t)
